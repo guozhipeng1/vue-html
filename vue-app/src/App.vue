11412133c872eaa715241d71ed7537d54970df28
guozhipeng1 <512127231@qq.com>
2019-12-30 08:24:41 +0800

    这是一个vue脚手架

---
+++ vue-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <<home></home>
+    <router-view/>
+  </div>
+</template>
+<script>
+	import home from './views/home.vue'
+	export default{
+		components:{
+			home:home
+		}
+	}
+</script>
+
+<style lang="scss">
+ 
+</style>
---
+++ vue-app/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+
+Vue.prototype.$bus=new Vue()
+Vue.use(swiper)
+Vue.config.productionTip = false
+
+new Vue({
+  // router,
+  render: h => h(App)
+}).$mount('#app')
